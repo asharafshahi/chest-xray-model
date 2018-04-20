66295341278260cd6426cae28483f68ad33c8478
Ubuntu <nuance@NuanceAI.dggjdzq41x3e5ci4xqilxovfff.bx.internal.cloudapp.net>
2018-04-20 21:09:04 +0000

    First commit of bundled chest model with wrapper

---
+++ app.py
@@ -0,0 +1,66 @@
+import numpy as np
+from flask import Flask, jsonify, request
+import os
+from configparser import ConfigParser
+from models.keras import ModelFactory
+import time
+import statistics
+import argparse
+from PIL import Image
+from skimage.transform import resize
+
+global model
+
+# parser config
+config_file = "./config.ini"
+cp = ConfigParser()
+cp.read(config_file)
+
+# default config
+output_dir = cp["DEFAULT"].get("output_dir")
+base_model_name = cp["DEFAULT"].get("base_model_name")
+class_names = cp["DEFAULT"].get("class_names").split(",")
+image_source_dir = cp["DEFAULT"].get("image_source_dir")
+
+# parse weights file path
+output_weights_name = cp["TRAIN"].get("output_weights_name")
+
+print(output_weights_name)
+best_weights_path = os.path.join(output_dir, f"best_{output_weights_name}")
+
+print("** load model **")
+model_weights_path = best_weights_path
+
+model_factory = ModelFactory()
+model = model_factory.get_model(
+    class_names,
+    model_name=base_model_name,
+    use_base_weights=False,
+    weights_path=model_weights_path)
+
+app = Flask(__name__)
+
+@app.route("/score/")
+def score_image():
+    filename = request.args['file']
+    print(f"Got file name {filename}")
+
+    #image_path = os.path.join(image_source_dir, filename)
+    #image = Image.open(image_path)
+    image = Image.open(filename) 
+    image_array = np.asarray(image.convert("RGB"))
+    image_array = image_array / 255.
+    image_array = resize(image_array, [224,224])
+    image_array = np.expand_dims(image_array, axis=0)
+
+    print("** make prediction **")
+    y_hat = model.predict(image_array, 1)
+    prediction = class_names[np.argmax(y_hat)]
+
+    print(f"Model score for image is: {y_hat}")
+    print(f"Highest probability is: {prediction}")
+
+    return jsonify(prediction)
+
+if __name__ == "__main__":    
+    app.run(port=8000)
---
+++ index.js
@@ -0,0 +1,77 @@
+const http = require('http');
+const axios = require('axios');
+const fs = require('fs-extra');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const dicom = require('dicom-parser/dist/dicomParser');
+require('dotenv').load();
+
+const imageRootDir = process.env.IMAGE_ROOT_DIR;
+const hostname = '0.0.0.0';
+const port = 3000;
+const modelEndpoint = 'http://127.0.0.1:8000/score/?file='
+
+const server = http
+  .createServer((req, res) => {
+    console.log(`\n${req.method} ${req.url}`);
+    console.log(req.headers);
+    let body = [];
+    req
+      .on('data', chunk => {
+        body.push(chunk);
+      })
+      .on('end', async () => {
+        body = Buffer.concat(body).toString();
+        response = JSON.parse(body);
+        let studyFolder;
+        await Promise.all(response.uris.map(async url => {
+          try {
+            const result = await axios.get(url, {responseType: 'arraybuffer'});
+            const { studyUid, imageUid } = getUids(result.data);
+            studyFolder = `${imageRootDir}/${studyUid}`;
+            const outputFilename = `${imageRootDir}/${studyUid}/${imageUid}.dcm`;
+            fs.ensureDirSync(studyFolder);
+            fs.writeFileSync(outputFilename, result.data);
+            console.log(`Wrote file ${outputFilename}`); 
+            await preProcessToPng(studyFolder);
+
+         } catch (err) {
+           console.error(err);
+         }
+       }));
+       const result = await runModel(studyFolder + '/preprocess');
+       console.log(result[0].data);
+    });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Acknowledged\n');
+  })
+  .listen(port, hostname, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
+
+const getUids = dicomData => {
+  const dataSet = dicom.parseDicom(dicomData);
+  const studyUid = dataSet.string('x0020000d');
+  const imageUid = dataSet.string('x00080018');
+  return { studyUid, imageUid };
+};
+
+const preProcessToPng = async directory => {
+  const fileList = fs.readdirSync(directory);
+  fs.ensureDirSync(directory + '/preprocess');
+  await Promise.all(fileList.map(file =>
+    exec(`gdcm2vtk ${directory + '/' + file} ${directory + '/preprocess/' +
+          file.substr(0,file.length - 4) + '.png'}`)
+  ));
+  return;
+}
+
+const runModel = async directory => {
+  const fileList = fs.readdirSync(directory);
+  return await Promise.all(fileList.map(file => {
+    const url =`${modelEndpoint}${directory + '/' + file}`; 
+    console.log(url);
+    return axios.get(url);
+  }));
+}
